62d20d2cd001dca881fcd6fd0da7070275645b2a
Florian Angermeir <florian@angermeir.com>
2020-10-19 01:23:22 +0200

    Add code for evaluation of RQ2

---
+++ 04.analysis/RQ2/01.RQ2.py
@@ -0,0 +1,45 @@
+import json
+import matplotlib.pyplot as plt
+from collections import defaultdict, Counter
+import numpy as np
+
+
+def load_project_security():
+    with open('../../results/03.tools_usage_detection/all_projects_security.json') as infile:
+        return json.load(infile)
+
+
+def load_sec_patterns():
+    with open('../../resources/security_patterns.json', 'r') as infile:
+        return json.load(infile)
+
+
+project_sec = load_project_security()
+security_act = load_sec_patterns()
+
+
+# Tools & Activities
+def visualize_tools(sec_projects):
+    tools = defaultdict(int)
+    for _, val in sec_projects.items():
+        for tool in val:
+            tools[tool] += 1
+
+    tools = {key: value for key, value in sorted(tools.items(), key=lambda item: item[1], reverse=True)}
+
+    print("Security tools used along with the number of projects using them:")
+    for tool, count in tools.items():
+        print(tool, count)
+
+    plt.bar(range(len(tools)), list(tools.values()), align='center')
+    plt.ylabel('Projects')
+    plt.xticks(range(len(tools)), list(tools.keys()), rotation=90, fontsize=5)
+    plt.savefig('../../results/visualization/tools_graph.png', format='png', dpi=500, bbox_inches='tight')
+
+
+def main():
+    visualize_tools(project_sec)
+
+
+if __name__ == "__main__":
+    main()
